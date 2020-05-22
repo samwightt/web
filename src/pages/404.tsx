07495778281d2ebc6acf3c245709df463e951d62
Sam Wight <samuelwight@gmail.com>
2020-05-21 23:04:49 -0500

    Add better 404 page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,18 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 import Layout from "../components/home-layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
+  <div className="h-screen w-screen bg-black flex flex-col justify-center items-center">
     <SEO title="404: Not found" />
-  </Layout>
+    <div className="flex flex-col items-center">
+      <h1 className="font-extrabold font-open text-white text-7xl -m-4">404</h1>
+      <h3 className="font-light font-open text-md text-white">That's an error.</h3>
+      <Link to="/" className="bg-white py-2 px-3 rounded-lg text-sm text-black font-open font-light mt-10">Go home</Link>
+    </div>
+  </div>
 )
 
 export default NotFoundPage
